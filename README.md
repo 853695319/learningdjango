e8e6577c85c3e731fa1a7e42337a7ebffc4029e0
C-zx <853695319@qq.com>
2019-03-31 02:51:30 +0800

    创建django项目文件目录，把mysql的配置myconfig和数据库mysqldata设置好，通过docker容器控制

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # learningdjango
 记录学习django的过程资料
+
+django_py3.6 python3.6虚拟环境
+test1 是django项目文件夹
---
+++ django_py3.6/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chenzhixiong/09-django/learningdjango/django_py3.6/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
