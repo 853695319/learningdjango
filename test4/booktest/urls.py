7c034dfe72abecad495e802619360b63767231a3
C-zx <853695319@qq.com>
2019-04-09 05:40:42 +0800

    MVT-模板：项目4 模板继承

--- test4/booktest/urls.py
+++ test4/booktest/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(\d+)/$', views.show, name='show'),
+
+    # 基于base.html
+    url(r'^index2/$', views.index2, name='index2'),
+
+    # base_user.html
+    url(r'^user1/$', views.user1, name='user1'),
+    url(r'^user2/$', views.user2, name='user2'),
 ]
--- test4/booktest/views.py
+++ test4/booktest/views.py
@@ -15,3 +15,17 @@ def index(request):
 def show(request, id_):
     context = {'id': id_}
     return render(request, 'booktest/show.html', context)
+
+
+# 模板继承
+def index2(request):
+    return render(request, 'booktest/index2.html')
+
+
+def user1(request):
+    context = {'uname': 'user1.html'}  # 那么这个变量可以用在 user.html,它的父模板也是可以用这个变量的
+    return render(request, 'booktest/user1.html', context)
+
+
+def user2(request):
+    return render(request, 'booktest/user2.html')
