054e6b274745be94890111d1d414a6201493152c
C-zx <853695319@qq.com>
2019-04-08 10:51:47 +0800

    MVT-模板：项目4 定义booktest.models

--- test4/booktest/models.py
+++ test4/booktest/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(db_column='pub_date')
+    bread = models.IntegerField(default=0)
+    bcomment = models.IntegerField(null=False)
+    isDelete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.btitle
+
+    class Meta:
+        db_table = 'bookinfo'
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField(default=True)
+    hcontent = models.CharField(max_length=1000)
+    idDelete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo)
+
+    def __str__(self):
+        return self.hname
