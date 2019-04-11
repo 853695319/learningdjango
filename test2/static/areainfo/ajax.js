a2c4d2685cbf0c6845f0d7ad2f82ede98ba1bc26
C-zx <853695319@qq.com>
2019-04-11 17:54:01 +0800

    MVT-高级：项目test2 ajax应用 完善效果

--- test2/static/areainfo/ajax.js
+++ test2/static/areainfo/ajax.js
@@ -1,6 +1,10 @@
 $(function () {
-    // 1 查询省
+
     var $pro = $('#pro');
+    var $city = $('#city');
+    var $dis = $('#dis')
+
+    // 1 查询省
     // 查询省信息，获得JSON格式的数据 data={'data':[]}
     $.get('/area-pro/', function(data) {
         console.log('get pro');
@@ -17,13 +21,13 @@ $(function () {
     });
 
     // 2 查询市
-    var $city = $('#city');
     // 根据用户选定省份，生成市列表
     $pro.change(function () {
         console.log('pro change');
 
         // 清楚之前的数据
         $city.empty().append('<option value="0">请选择市</option>');
+        $dis.empty().append('<option value="0">请选择区</option>');
 
         // $(this) -> 当前选中的selectElement，不是optionElement
         var url = '/area-city-'+$(this).val()+'/';
@@ -36,7 +40,6 @@ $(function () {
     });
 
     // 3 查询区，根据用户选定的城市查询区
-    var $dis = $('#dis')
     $city.change(function () {
         // 清楚之前的数据
         $dis.empty().append('<option value="0">请选择区</option>');
@@ -45,7 +48,7 @@ $(function () {
         var url = '/area-city-'+$(this).val()+'/';
         $.get(url, function (data) {
             $.each(data.data, function (index, item) {
-                $dis.append('<option value="' + item.id + '">' + item.title + '</option>');
+                $dis.append('<option>' + item.title + '</option>');
             });
         });
     });
